dfb88c71a09aa3b6abb193c5eb3e67cab526a842
piscesmk2013 <piscesmk2013@gmail.com>
2013-04-07 21:38:26 +0800

    fix vendor path
    Change-Id: I5b1b7d2d161827df2fb56540d5911d72bcb27082

--- jordan-vendor.mk
+++ jordan-vendor.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-vendor_path = vendor/motorola/jordan-common
+vendor_path = vendor/motorola
 
 # LIBS
 PRODUCT_COPY_FILES += \
